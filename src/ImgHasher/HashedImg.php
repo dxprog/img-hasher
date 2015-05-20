e1a07ddf543ee3bcda03a4eea6918370068c3247
Matt Hackmann <matt@dxprog.com>
2015-05-19 23:02:22 -0700

    initial commit

---
+++ src/ImgHasher/HashedImg.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * A simple image hashing and comparison class based upon
+ * the following articles found at hackerfactor.com
+ * aHash: http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html
+ * dHash: http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html
+ */
+
+namespace ImgHasher {
+
+    const RGB_MAX = 255;
+
+    // Image scaling dimensions for dHash
+    const DHASH_WIDTH = 9;
+    const DHASH_HEIGHT = 8;
+
+    class HashedImg {
+
+        private $_img = null;
+
+        // The cached dHash value
+        private $_dHash = null;
+
+        public function __construct($img) {
+            $this->_validateImage($img);
+            $this->_img = $img;
+        }
+
+        public function aHash($img) {
+            // TODO
+        }
+
+        /**
+         * Returns the distance hash of the image
+         * @return int The image hash
+         */
+        public function dHash() {
+
+            // Return the cached dHash value if it's there
+            if ($this->_dHash) {
+                return $this->_dHash;
+            }
+
+            $img = $this->_img;
+
+            // We need to have 64-bit numbers available for this to work
+            if (PHP_INT_SIZE !== 8) {
+                throw new Exception('dHash requires 64-bit integer support');
+            }
+
+            // The original image is resampled down to 9x8.
+            // We'll build our bit hash from this
+            $tempImg = imagecreatetruecolor(9, 8);
+            imagecopyresampled($tempImg, $img, 0, 0, 0, 0, DHASH_WIDTH, DHASH_HEIGHT, imagesx($img), imagesy($img));
+
+            $retVal = 0;
+            for ($y = 0; $y < DHASH_HEIGHT; $y++) {
+                for ($x = 0; $x < DHASH_WIDTH; $x++) {
+                    $luma = $this->_calculateLuminence(imagecolorat($tempImg, $x, $y));
+                    // We ignore the first pixel in the row
+                    if ($x > 0) {
+                        // The value for this pixel is whether it was brighter than the previous
+                        // We then put that at bit slot 0
+                        $retVal = $retVal << 1;
+                        $retVal |= $luma < $lastLuma ? 1 : 0;
+                    }
+                    $lastLuma = $luma;
+                }
+            }
+
+            imagedestroy($tempImg);
+            unset($tempImg);
+
+            return $retVal;
+        }
+
+        /**
+         * Performs a dHash compare between this image and another image
+         * @param mixed $img A GD2 resource, HashedImg object, or numeric hash
+         * @return int The distance between the two images
+         */
+        public function dHashCompare($img) {
+            if ($img instanceof HashedImg) {
+                $dHash = $img->dHash();
+            } else if (is_numeric($img)) {
+                $dHash = $img;
+            } else if ($this->_validateImage($img)) {
+                $tempHash = new HashedImg($img);
+                $dHash = $tempHash->dHash();
+                unset($tempHash);
+            } else {
+                throw new Exception('Invalid image parameter passed to dHashCompare');
+            }
+            return $this->_calculateHammingDistance($this->dHash(), $dHash);
+        }
+
+        /**
+         * Given an RGB color, calculates it's luminence value
+         */
+        private function _calculateLuminence($color) {
+            $r = ($color >> 16) / RGB_MAX;
+            $g = (($color >> 8) & 0xff) / RGB_MAX;
+            $b = ($color & 0xff) / RGB_MAX;
+            return 0.2126 * $r + 0.7152 * $g + 0.0722 * $b;
+        }
+
+        private function _calculateHammingDistance($hash1, $hash2) {
+            $retVal = 0;
+            for ($i = 0, $count = PHP_INT_SIZE * 8; $i < $count; $i++) {
+                $retVal += (($hash1 >> $i) & 0x1) ^ (($hash2 >> $i) & 0x1);
+            }
+            return $retVal;
+        }
+
+        private function _validateImage($img) {
+            if (!$img || false === imagesx($img)) {
+                throw new Exception('Invalid image');
+            }
+            return true;
+        }
+
+    }
+
+}
